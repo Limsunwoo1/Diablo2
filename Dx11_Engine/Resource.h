699637a6dfd3ac884caa5d4d1cdfc84f9e275f5c
Limsunwoo1 <tjsdnhj@gmail.com>
2023-04-05 05:08:40 +0900

    상대경로

--- Dx11_Engine/Resource.h
+++ Dx11_Engine/Resource.h
@@ -14,7 +14,6 @@
 #define IDI_SMALL                       108
 #define IDC_DX11ENGINE                  109
 #define IDR_MAINFRAME                   128
-#define IDC_CURSOR1                     132
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -22,7 +21,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        133
+#define _APS_NEXT_RESOURCE_VALUE        134
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
