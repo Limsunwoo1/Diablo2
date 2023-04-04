8c635137d243f41369418a3cc8b4a922e1a8bf5e
Limsunwoo1 <tjsdnhj@gmail.com>
2023-04-05 04:57:52 +0900

    커서 변경

--- Dx11_Engine/Resource.h
+++ Dx11_Engine/Resource.h
@@ -2,7 +2,6 @@
 // Microsoft Visual C++에서 생성한 포함 파일입니다.
 // Dx11_Engine.rc에서 사용되고 있습니다.
 //
-#define _APS_NO_MFC                     1
 #define IDC_MYICON                      2
 #define IDC_MY_CURSOR1                  101
 #define IDD_DX11ENGINE_DIALOG           102
@@ -14,11 +13,8 @@
 #define IDI_DX11ENGINE                  107
 #define IDI_SMALL                       108
 #define IDC_DX11ENGINE                  109
-#define _APS_NEXT_SYMED_VALUE           110
 #define IDR_MAINFRAME                   128
-#define _APS_NEXT_RESOURCE_VALUE        132
-#define _APS_NEXT_CONTROL_VALUE         1000
-#define _APS_NEXT_COMMAND_VALUE         32771
+#define IDC_CURSOR1                     132
 #define IDC_STATIC                      -1
 
 // Next default values for new objects
@@ -26,7 +22,7 @@
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NO_MFC                     1
-#define _APS_NEXT_RESOURCE_VALUE        132
+#define _APS_NEXT_RESOURCE_VALUE        133
 #define _APS_NEXT_COMMAND_VALUE         32771
 #define _APS_NEXT_CONTROL_VALUE         1000
 #define _APS_NEXT_SYMED_VALUE           110
