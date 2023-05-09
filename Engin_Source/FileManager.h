d73b472a52feecd26f3b5d15097cce1055c2f870
Limsunwoo1 <tjsdnhj@gmail.com>
2023-05-10 04:04:31 +0900

    플레이어 데이터 세팅

--- Engin_Source/FileManager.h
+++ Engin_Source/FileManager.h
@@ -23,8 +23,7 @@ public:
 	const wstring& GetData(eFileType type, UINT index) { return mData[(UINT)type][index];}
 	UINT GetDataSize_Int(eFileType type) { return mData[(UINT)type].size(); }
 
-
-	
+	void SetPlayerData(const wstring& data) { mPlayerData = data; }
 private:
 	vector<wstring> mDataPath[(UINT)eFileType::End];
 	vector<wstring> mData[(UINT)eFileType::End];
